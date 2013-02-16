55948c721a1046bbfae0d883cb740a42701d294c
turbogus <hooover@hotmail.fr>
2013-02-16 11:04:27 +0100

    changement licence
    code gpl v2 ou sup
    graphisme CC-BY-NC-SA

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- Sève
 --
--- Licence GPL
+-- code licence gpl v2 ou superieur
+-- graphisme sous licence CC-BY-NC-SA
 -- 
 -- Par Jat et Turbogus
 --
@@ -86,7 +87,7 @@ minetest.register_node("fluide:seve_source", {			-- Declaration du nom de l'item
 --*****************************************
 
 minetest.register_abm(									-- fonction permettant d'assigner une action à un bloc
-    {nodenames = {"fluide:seve_flowing"},							-- nom du bloc
+    {nodenames = {"fluide:seve_flowing","fluide:seve_source"},							-- nom du bloc
     interval = 1.0,									-- ???
     chance = 1,										-- ???
     action = function(pos, node, active_object_count, active_object_count_wider)	-- fonction associée au bloc
@@ -99,6 +100,7 @@ minetest.register_abm(									-- fonction permettant d'assigner une action à u
 })
 
 
+--=========================================================================================================================================================
 --=========================================================================================================================================================
 
 -- OR 
@@ -129,7 +131,8 @@ minetest.register_node("fluide:or_flowing", {			-- on enregistre le nom de l'ite
 	liquidtype = "flowing",					-- //
 	liquid_alternative_flowing = "fluide:or_flowing",	-- //
 	liquid_alternative_source = "fluide:or_source",		-- //
-	liquid_viscosity = WATER_VISC,				-- //
+	liquid_viscosity = WATER_VISC,
+	damage_per_second = 20*2,
 	post_effect_color = {a=64, r=100, g=100, b=200},	-- //
 	groups = {water=3, liquid=3, puts_out_fire=1},		-- //
 	
@@ -160,47 +163,141 @@ minetest.register_node("fluide:or_source", {			-- Declaration du nom de l'item
 	liquidtype = "source",
 	liquid_alternative_flowing = "fluide:or_flowing",	-- //
 	liquid_alternative_source = "fluide:or_source",		-- //
-	liquid_viscosity = WATER_VISC,				-- //
+	liquid_viscosity = WATER_VISC,
+	damage_per_second = 20*2,
 	post_effect_color = {a=64, r=100, g=100, b=200},	-- //
 	groups = {water=3, liquid=3, puts_out_fire=1},		-- //
 })
 
+--=========================================================================================================================================================
+--=========================================================================================================================================================
 
 
---**********************************************
--- On se fait tuer si on touche une source d'Or
---**********************************************
+--gazole
 
-minetest.register_abm(									-- fonction permettant d'assigner une action à un bloc
-    {nodenames = {"fluide:or_flowing"},							-- nom du bloc
-    interval = 1.0,									-- ???
-    chance = 1,										-- ???
-    action = function(pos, node, active_object_count, active_object_count_wider)	-- fonction associée au bloc
-    local objs = minetest.env:get_objects_inside_radius(pos, 1)				-- variable d'environnement : quand le joueur est dans le bloc
-        for k, obj in pairs(objs) do							-- boucle "for" : 
-        obj:set_hp(obj:get_hp()-20)     -- give 1HP					-- augmenter la vie HP du joueur de +1
-    end
-    end,
-     
+-- Liquide extremement inflammable utilise en combustible dans les fours
+
+minetest.register_node("fluide:gazole_flowing", {			-- on enregistre le nom de l'item
+	description = "Flowing gazole",				-- description dans le menu
+	inventory_image = minetest.inventorycube("gazole.png"),	-- on charge la skin du bloc dans l'inventaire du joueur
+	drawtype = "flowingliquid",				-- type de bloc : ecoulement de liquide
+	tiles = {"gazole.png"},					-- on charge la skin du bloc dans le jeu
+	special_tiles = {
+		{name="gazole.png", backface_culling=false},	-- parametres speciaux
+		{name="gazole.png", backface_culling=true},	-- //
+	},
+	alpha = WATER_ALPHA,					-- paramètre du liquide
+	paramtype = "light",
+	light_source = LIGHT_MAX - 1,					-- //
+	walkable = false,					-- //
+	pointable = true,					-- //
+	diggable = false,					-- //
+	buildable_to = true,					-- //
+	liquidtype = "flowing",					-- //
+	liquid_alternative_flowing = "fluide:gazole_flowing",	-- //
+	liquid_alternative_source = "fluide:gazole_source",		-- //
+	liquid_viscosity = WATER_VISC,				-- //
+	post_effect_color = {a=64, r=100, g=100, b=200},	-- //
+	groups = {water=3, liquid=3, flammable=1},		-- //
+	
+	
 })
 
 
+--***********************************************
+--Gestion et cretion d'un bloc source de liquide
+--***********************************************
+
+minetest.register_node("fluide:gazole_source", {			-- Declaration du nom de l'item
+	description = "gazole Source",				-- description dans l'inventaire
+	inventory_image = minetest.inventorycube("gazole.png"),	-- chargement de la skin pour l'inventaire
+	drawtype = "liquid",					-- type de bloc : liquide
+	tiles = {"gazole.png"},					-- chargement skin pour jeu
+	special_tiles = {
+		-- New-style water source material (mostly unused)
+		{name="gazole.png", backface_culling=false},
+	},
+	alpha = WATER_ALPHA,					-- parametres du bloc
+	paramtype = "light",
+	light_source = LIGHT_MAX - 1,				-- Le fluide Or emet de la lumiere
+	walkable = false,					-- //
+	pointable = true,					-- //
+	diggable = false,					-- //
+	buildable_to = true,					-- //
+	liquidtype = "source",
+	liquid_alternative_flowing = "fluide:gazole_flowing",	-- //
+	liquid_alternative_source = "fluide:gazole_source",	-- //
+	liquid_viscosity = WATER_VISC,				-- //
+	post_effect_color = {a=64, r=100, g=100, b=200},	-- //
+	groups = {water=3, liquid=3, flammable=1},		-- //
+})
+
+--minetest.register_abm(									-- fonction permettant d'assigner une action à un bloc
+--    {nodenames = {"fluide:gazole_flowing"},							-- nom du bloc
+--    interval = 1.0,									-- ???
+--    chance = 1,										-- ???
+--    action = function(pos, node, active_object_count, active_object_count_wider)	-- fonction associée au bloc
+--    local objs = minetest.env:get_objects_inside_radius(pos, 1)				-- variable d'environnement : quand le joueur est dans le bloc
+--        for k, obj in pairs(objs) do							-- boucle "for" : 
+--        obj:set_hp(obj:get_hp()-0.5)     					
+--    end
+--    end,
+     
+--})
+
+-------------------
+-- test pour bucket
+-------------------
+
+bucket = {}
+bucket.liquids = {}
+
+function bucket.register_liquid(source, flowing, itemname, inventory_image)
+	bucket.liquids[source] = {
+		source = "fluide:gazole_source",
+		flowing = "fluide:gazole_flowing",
+		itemname = "fluide:bucket_gazole",
+		inventory_image = "bucket_gazole.png",
+	}
+	bucket.liquids[flowing] = bucket.liquids[source]
+
+	if itemname ~= nil then
+		minetest.register_craftitem(itemname, {
+			inventory_image = inventory_image,
+			stack_max = 1,
+			liquids_pointable = true,
+			on_use = function(itemstack, user, pointed_thing)
+				-- Must be pointing to node
+				if pointed_thing.type ~= "node" then
+					return
+				end
+				-- Check if pointing to a liquid
+				n = minetest.env:get_node(pointed_thing.under)
+				if bucket.liquids[n.name] ~= nil then
+					-- Not a liquid
+					minetest.env:add_node(pointed_thing.above, {name=source})
+				elseif n.name == source then
+					-- It's a liquid
+					minetest.env:add_node(pointed_thing.under, {name=source})
+				end
+				return {name="bucket:bucket_empty"}
+			end
+		})
+	end
+end
+
+bucket.register_liquid(
+
+	"fluide:gazole_source",
+	"fluide:gazole_flowing",
+	"fluide:gazole_bucket",
+	"bucket_gazole.png"
+	
+	--"default:water_source",
+	--"default:water_flowing",
+	--"bucket:bucket_water",
+	--"bucket_water.png"
+)
 
 
---*****
---NOTES
---*****
 
---1- Creer un craft (assez difficile ! ) permettant de creer une source de sève
-
---2- Permettre de recuperer de la sève dans un seau
-
---3- Utiliser la sève récupérée dans le seau poue crafter des élements ( lesquels ? A trouver ! :-) )
-
---**********
---CHANGE LOG
---**********
-
--- 25/07/2012 : - Ajout de la variable LIGHT_MAX = 14 
---              - Ajout du liquide Or ( tue en 1 seul coup
---		- Ajout de light_source = LIGHT_MAX - 3, à la sources de sève ( la seve est lumineuse )
